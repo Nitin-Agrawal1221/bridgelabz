18350c148da4a5f01c118fa46249947483b8640c
Nitin-Agrawal1221 <nitinagrawal7830@gmail.com>
2025-10-13 13:37:01 +0530

    Nitinagrawal-43

---
+++ background jod.java
@@ -0,0 +1,17 @@
+public class BackgroundJob {
+    public static void main(String[] args) 
+        Runnable job = () -> {
+            System.out.println("🕒 Background job started...");
+            try {
+                Thread.sleep(2000); 
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(" Background job completed!");
+        };
+        Thread thread = new Thread(job);
+        thread.start();
+
+        System.out.println(" Main thread continues executing");
+    }
+}
