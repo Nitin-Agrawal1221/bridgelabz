4acc3bb70e116cc29efae2228ab99b535af26bb8
Nitin-Agrawal1221 <nitinagrawal7830@gmail.com>
2025-11-14 15:38:00 +0530

    [nitin agrawal]/43

---
+++ count even numbers in array.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        System.out.println("Enter " + n + " elements:");
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int count = 0;
+
+        for(int i = 0; i < n; i++){
+            if(arr[i] % 2 == 0){
+                count++;
+            }
+        }
+
+        System.out.println("Total even numbers = " + count);
+    }
+}
